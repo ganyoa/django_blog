264d9e34c32d80ea67ec2b38c32967eb8c2b0670
Anthony Ganyo <ganyoa@gmail.com>
2021-03-23 13:35:54 -0700

    poll and blogging app review

--- blogging/models.py
+++ blogging/models.py
@@ -4,7 +4,10 @@ from django.contrib.auth.models import User
 class Post(models.Model):
     title = models.CharField(max_length=128)
     text = models.TextField(blank=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE) #tied to django User model, if user deleted remove all posts
     created_date = models.DateTimeField(auto_now_add=True)
     modified_date = models.DateTimeField(auto_now=True)
-    published_date = models.DateTimeField(blank=True, null=True)
+    published_date = models.DateTimeField(blank=True, null=True) # if empty or null not visible to public
+
+    #def __str__(self):
+        #return self.title
